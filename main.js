1a39e326c4f0bcbc7e3222222ff404e17e8cc041
Misha Manalo <manalo.mb@obmontessori.edu.ph>
2026-02-24 09:02:08 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function checkDiscount(){
+    let budget = document.getElementById("budget").value;
+    let discValue = budget *  .2;
+    
+    // display with JS Method
+    document.getElementById("discount").innerHTML = ''
+     <div class="container" id="discount-box"><h2>Recommended Brands</h2><div class="container text-center d-block mt-2"><button type="button" class="btn btn-outline-primary">Asus</button><button type="button" class="btn btn-outline-primary">Samsung</button><button type="button" class="btn btn-outline-primary">Lenovo</button></div><h5>Eligble Discount <span class="badge bg-danger mt-3">40000.00</span></h5>
+       
+
+}
